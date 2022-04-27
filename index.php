0319e8c3958d8de1814e51b5e1bd73fa7969f876
Renan Cavichi <renancavichi@gmail.com>
2022-04-27 17:00:05 -0300

    Add html and css to show products

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>CRUD PDO</title>
+		<link rel="stylesheet" href="css/style.css" />
+	</head>
+	<body>
+		<main>
+				<header>
+					<h1>Products</h1>
+					<button>Add Product</button>
+				</header>
+				<section id="product-list">
+					<div class="product">
+					</div>
+					<div class="product">
+					</div>
+					<div class="product">
+					</div>
+					<div class="product">
+					</div>
+					<div class="product">
+					</div>
+					<div class="product">
+					</div>
+				</section>
+		</main>
+	</body>
+</html>
